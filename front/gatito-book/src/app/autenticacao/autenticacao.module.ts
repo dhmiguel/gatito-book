850123baaf9555aa810e263fc5041f3c1481106e
Diego Miguel <diegomiguel.201@gmail.com>
2023-05-05 20:15:13 -0300

    Add registro do interceptor

--- front/gatito-book/src/app/autenticacao/autenticacao.module.ts
+++ front/gatito-book/src/app/autenticacao/autenticacao.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AutenticacaoInterceptor } from './autenticacao.interceptor';
 
 
 
@@ -7,6 +9,13 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: AutenticacaoInterceptor,
+      multi: true, //Indica que pode ter uma cadeia de interceptors
+    }
   ]
 })
 export class AutenticacaoModule { }
