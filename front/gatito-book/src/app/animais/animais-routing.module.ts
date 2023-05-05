da375a08dd7f8327979fab6805b60424b3a44942
Diego Miguel <diegomiguel.201@gmail.com>
2023-05-05 19:48:26 -0300

    Add tep=mplate detalhe animal

--- front/gatito-book/src/app/animais/animais-routing.module.ts
+++ front/gatito-book/src/app/animais/animais-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
   },
+
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent
+
+  }
 ];
 
 @NgModule({
