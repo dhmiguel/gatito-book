9faac38d20082d29aa34453ce517af83d9783ec7
Diego Miguel <diegomiguel.201@gmail.com>
2023-05-08 19:42:38 -0300

    Add esqueleto inicial template comentarios

--- front/gatito-book/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ front/gatito-book/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comentarios } from './comentarios';
 
 @Component({
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
   styleUrls: ['./comentarios.component.css']
 })
-export class ComentariosComponent {
+export class ComentariosComponent implements OnInit {
+
+  comentarios$!: Observable<Comentarios>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 
 }
